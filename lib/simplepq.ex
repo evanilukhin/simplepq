0a99e8f9c5918fb586f11a7710084145cb86537f
Evan Ilukhin <evanilukhin@gmail.com>
2018-10-10 07:26:48 +0300

    Init repository

---
+++ lib/simplepq.ex
@@ -0,0 +1,18 @@
+defmodule Simplepq do
+  @moduledoc """
+  Documentation for Simplepq.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Simplepq.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/simplepq_test.exs
@@ -0,0 +1,8 @@
+defmodule SimplepqTest do
+  use ExUnit.Case
+  doctest Simplepq
+
+  test "greets the world" do
+    assert Simplepq.hello() == :world
+  end
+end
